beb48e4dfac3b92cf7f2b387e55c447d4bd0466a
Hemil <hemilparmar1005@gmail.com>
2021-08-24 22:50:43 +0530

    fInal2

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -34,6 +34,7 @@ export default function TextForm(props) {
         let text = document.getElementById("myBox")
         text.select()
         navigator.clipboard.writeText(text.value)
+        document.getSelection().removeAllRanges()
         props.showAlert("Copied to Clipboard!", "success")
     }
 
